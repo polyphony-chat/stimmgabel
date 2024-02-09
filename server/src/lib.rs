3a7c6db13fd477c79e17000ffa3bc38a89913a9b
bitfl0wer <florian@pro-weber.com>
2024-02-09 22:56:33 +0100

    set up cli in root of crate

---
+++ server/src/lib.rs
@@ -0,0 +1,29 @@
+use axum::{
+    routing::{get, post, put},
+    Router,
+};
+
+async fn run() {
+    let server = Router::new()
+        .route("/", get(hello_world))
+        .route("/p2core/session/trust", post(hello_world))
+        .route("/p2core/session/idcert", post(hello_world))
+        .route("/p2core/keypackage/@me", post(hello_world))
+        .route("/p2core/keypackage_lr", put(hello_world))
+        .route("/p2core/register", post(hello_world))
+        .route("/p2core/challenge", get(hello_world))
+        .route("/p2core/session/identify", post(hello_world))
+        .route("/p2core/session/revoke", put(hello_world))
+        .route("/p2core/key/server", put(hello_world))
+        .route("/p2core/key/server", get(hello_world))
+        .route("/p2core/key/actor/:fid", get(hello_world))
+        .route("/p2core/session/idcert/extern", post(hello_world))
+        .route("/p2core/keypackage/:fid", get(hello_world))
+        .route("/p2core/events", get(hello_world));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, server).await.unwrap();
+}
+
+async fn hello_world() -> String {
+    String::from("hello, world!")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(long, short)]
+    mode: Mode,
+    #[arg(long, short)]
+    port: Option<u16>,
+}
+
+#[derive(Clone, Copy, ValueEnum)]
+enum Mode {
+    Client,
+    Server,
+}
+
 fn main() {
+    let args = Cli::parse();
     println!("Hello, world!");
 }
