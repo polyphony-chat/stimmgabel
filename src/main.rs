b4909637e8725c5e32f4846c893198dfd2389774
bitfl0wer <florian@pro-weber.com>
2024-02-10 18:07:31 +0100

    Add ascii art, ports, verbose mode

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use clap::{Parser, ValueEnum};
 
+mod ascii;
+
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
     #[arg(long, short)]
     mode: Mode,
+    /// Port for the server to listen on, or for the client to connect to.
+    #[arg(long, short, default_value_t = 4001)]
+    port: u16,
     #[arg(long, short)]
-    port: Option<u16>,
+    verbose: bool,
+    #[arg(long, short)]
+    waves: bool,
 }
 
 #[derive(Clone, Copy, ValueEnum)]
@@ -18,9 +25,12 @@ enum Mode {
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let args = Cli::parse();
-    println!("Hello, world!");
+    std::env::set_var("RUST_LOG", if args.verbose { "DEBUG" } else { "" });
+    if args.waves {
+        ascii::ascii_waves()
+    }
     match args.mode {
-        Mode::Client => client::run(args.port.unwrap_or(4001)),
-        Mode::Server => server::run(args.port.unwrap_or(4001)).await,
+        Mode::Client => client::run(args.port),
+        Mode::Server => server::run(args.port).await,
     }
 }
