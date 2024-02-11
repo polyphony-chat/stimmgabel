fbdcef768dc69dc22702fa9bc0a8708ad90aa654
bitfl0wer <florian@pro-weber.com>
2024-02-11 12:44:20 +0100

    add testcase struct

---
+++ src/testcase.rs
@@ -0,0 +1,65 @@
+#[cfg(not(test))]
+use crate::CLI_ARGS;
+
+#[cfg(test)]
+static CLI_ARGS: crate::Cli = crate::Cli {
+    mode: crate::Mode::Server,
+    port: 8192,
+    verbose: false,
+    waves: false,
+};
+
+/// Represents a `stimmgabel` test acceptance case.
+pub struct TestSync {
+    pub name: String,
+    pub description: String,
+    pub method: Box<dyn Fn(u16) -> Result<(), String>>,
+    result: Option<Result<(), String>>,
+}
+
+impl TestSync {
+    pub fn new(
+        name: &str,
+        description: &str,
+        method: Box<dyn Fn(u16) -> Result<(), String>>,
+    ) -> Self {
+        TestSync {
+            name: name.to_string(),
+            description: description.to_string(),
+            method,
+            result: None,
+        }
+    }
+
+    pub fn execute(&mut self) {
+        match (self.method)(CLI_ARGS.port) {
+            Ok(_) => self.result = Some(Ok(())),
+            Err(e) => self.result = Some(Err(e)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn port_is_power_of_two(port: u16) -> Result<(), String> {
+        if port.is_power_of_two() {
+            Ok(())
+        } else {
+            Err("The supplied port is not a power of two.".to_string())
+        }
+    }
+
+    #[test]
+    fn test_test_struct() {
+        let mut test = TestSync::new(
+            "My synchronous Test",
+            "a synchronous test",
+            Box::new(port_is_power_of_two),
+        );
+
+        test.execute();
+        assert!(test.result.is_some());
+    }
+}
