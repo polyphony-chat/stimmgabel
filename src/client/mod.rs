915fa450c8189c6fa5a105ab8b708eae8e9e4240
bitfl0wer <florian@pro-weber.com>
2024-02-13 21:02:38 +0100

    add random_name method

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -4,6 +4,39 @@
 
 mod routes;
 
+use rand::prelude::Rng;
+
 pub fn run(_port: u16) {
     println!("Hello, world!");
+}
+
+pub(super) fn random_name() -> String {
+    let names = [
+        "Cottage",
+        "Baguette",
+        "Xenia",
+        "Mechanist",
+        "Legibly",
+        "Scrounger",
+        "Unwed",
+        "Wed",
+        "Shady",
+        "Amusing",
+        "Deduce",
+        "Deduct",
+        "Excused",
+        "Infer",
+        "Rusty",
+        "Okay",
+    ];
+    let mut numbers = Vec::<u8>::with_capacity(4);
+    let mut rng = rand::thread_rng();
+    for _ in 0..4 {
+        numbers.push(rng.gen_range(0..=9))
+    }
+    format!(
+        "{}{}",
+        names.get(rng.gen_range(0..=15)).unwrap(),
+        numbers.iter().map(|x| x.to_string()).collect::<String>()
+    )
 }
