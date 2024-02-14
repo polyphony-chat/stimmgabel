a3b03e38939345586738c711d38c482a667267f4
bitfl0wer <florian@pro-weber.com>
2024-02-14 18:08:07 +0100

    correct my spelling of "ed25519"

--- src/cryptography/ed25119/mod.rs
+++ src/cryptography/ed25119/mod.rs
@@ -6,29 +6,29 @@ use asn1_rs::{Any, Oid};
 use polyproto::{PrivateKey, PublicKey, Signature, SignatureAlgorithm};
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Ed25119 {
+pub struct Ed25519 {
     oid: Oid<'static>,
     parameters: Option<Any<'static>>,
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Ed25119Signature {
-    algorithm: Ed25119,
+pub struct Ed25519Signature {
+    algorithm: Ed25519,
     signature: Vec<u8>,
 }
 
 #[derive(Clone, Debug)]
-pub struct Ed25119PrivateKey {
-    pub_key: Ed25119PublicKey,
+pub struct Ed25519PrivateKey {
+    pub_key: Ed25519PublicKey,
     key: Vec<u8>,
 }
 
 #[derive(Clone, Debug)]
-pub struct Ed25119PublicKey {
+pub struct Ed25519PublicKey {
     key: Vec<u8>,
 }
 
-impl SignatureAlgorithm for Ed25119 {
+impl SignatureAlgorithm for Ed25519 {
     fn oid(&self) -> Oid {
         self.oid.clone()
     }
@@ -38,43 +38,43 @@ impl SignatureAlgorithm for Ed25119 {
     }
 
     fn name(&self) -> &str {
-        "ED25119"
+        "Ed25519"
     }
 }
 
-impl Signature<Ed25119> for Ed25119Signature {
+impl Signature<Ed25519> for Ed25519Signature {
     fn signature(&self) -> &[u8] {
         &self.signature
     }
 
-    fn algorithm(&self) -> Ed25119 {
+    fn algorithm(&self) -> Ed25519 {
         self.algorithm.clone()
     }
 }
 
-impl PrivateKey<Ed25119> for Ed25119PrivateKey {
-    type PublicKey = Ed25119PublicKey;
-    type Signature = Ed25119Signature;
+impl PrivateKey<Ed25519> for Ed25519PrivateKey {
+    type PublicKey = Ed25519PublicKey;
+    type Signature = Ed25519Signature;
     fn to_bytes(&self) -> &[u8] {
         todo!()
     }
 
-    fn pubkey(&self) -> Ed25119PublicKey {
+    fn pubkey(&self) -> Ed25519PublicKey {
         todo!()
     }
 
-    fn sign(&self, data: &[u8]) -> Ed25119Signature {
+    fn sign(&self, data: &[u8]) -> Ed25519Signature {
         todo!()
     }
 }
 
-impl PublicKey<Ed25119> for Ed25119PublicKey {
-    type Signature = Ed25119Signature;
+impl PublicKey<Ed25519> for Ed25519PublicKey {
+    type Signature = Ed25519Signature;
     fn to_bytes(&self) -> &[u8] {
         todo!()
     }
 
-    fn verify_signature(&self, signature: Ed25119Signature, data: &[u8]) -> bool {
+    fn verify_signature(&self, signature: Ed25519Signature, data: &[u8]) -> bool {
         todo!()
     }
 }
