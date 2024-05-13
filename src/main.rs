d056eb3d2912f93ff49c8b6182621ca9cc5083d7
bitfl0wer <florian@pro-weber.com>
2024-05-13 11:19:50 +0200

    reset project

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,9 @@
+// Copyright (c) 2024 bitfl0wer
+//
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use clap::{Parser, ValueEnum};
-use colored::Color;
-use lazy_static::lazy_static;
+mod polyproto;
 
-mod ascii;
-#[cfg(feature = "client")]
-mod client;
-mod cryptography;
-#[cfg(feature = "server")]
-mod server;
-mod testcase;
-
-lazy_static! {
-    static ref CLI_ARGS: Cli = Cli::parse();
-    static ref COLORS_PASS: (Color, Color) = match CLI_ARGS.colorblind {
-        true => (Color::BrightBlue, Color::Blue),
-        false => (Color::BrightGreen, Color::Green),
-    };
-    static ref COLORS_FAIL: (Color, Color) = match CLI_ARGS.colorblind {
-        true => (Color::BrightYellow, Color::Yellow),
-        false => (Color::BrightRed, Color::Red),
-    };
-}
-
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-pub struct Cli {
-    #[arg(long, short)]
-    mode: Mode,
-    /// Port for the server to listen on, or for the client to connect to.
-    #[arg(long, short, default_value_t = 4001)]
-    port: u16,
-    #[arg(long, short)]
-    verbose: bool,
-    #[arg(long, short)]
-    waves: bool,
-    /// Changes the set of "fail/pass" colors to more colorblind-friendly ones.
-    #[arg(long, default_value_t = std::env::var("COLORBLIND").is_ok())]
-    colorblind: bool,
-    /// The URL the client should connect to.
-    #[arg(short, long, default_value_t = String::from("http://localhost"))]
-    url: String,
-}
-
-#[derive(Clone, Copy, ValueEnum)]
-enum Mode {
-    Client,
-    Server,
-}
-
-#[tokio::main]
-async fn main() {
-    std::env::set_var("RUST_LOG", if CLI_ARGS.verbose { "DEBUG" } else { "" });
-    if CLI_ARGS.waves {
-        ascii::ascii_waves()
-    }
-    match CLI_ARGS.mode {
-        Mode::Client => client::run(CLI_ARGS.port),
-        Mode::Server => server::run(CLI_ARGS.port).await,
-    }
-}
+fn main() {}
