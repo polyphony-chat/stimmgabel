954c7198a8f5f52ae9edb6f663d0358d878452be
bitfl0wer <florian@pro-weber.com>
2024-05-14 18:45:15 +0200

    Construct verification matching

--- src/commands/verify.rs
+++ src/commands/verify.rs
@@ -8,5 +8,62 @@ use crate::cli::{Format, StimmgabelMode};
 /// polyproto value. This function returns an exit code that can be used to signal the result of the
 /// verification.
 pub(crate) fn verify_input(mode: StimmgabelMode, value: &str, format: Format) -> i32 {
+    // Due to how complex this function would be if we were to implement all the verification
+    // logic here, we branch off here
+    match mode {
+        StimmgabelMode::ActorCertificate | StimmgabelMode::HomeserverCertificate => {
+            verify_certificate(value, format)
+        }
+        StimmgabelMode::Message => verify_message(value, format),
+        StimmgabelMode::ActorCsr | StimmgabelMode::HomeserverCsr => verify_csr(value, format),
+        StimmgabelMode::ActorDn | StimmgabelMode::HomeserverDn => verify_dn(value, format),
+        StimmgabelMode::ActorRdn | StimmgabelMode::HomeserverRdn => verify_rdn(value, format),
+    }
+}
+
+/// Verify the well-formedness as well as the syntactical and cryptographical correctness of a given
+/// certificate value. This function returns an exit code that can be used to signal the result of the
+/// verification.
+fn verify_certificate(value: &str, format: Format) -> i32 {
+    match format {
+        Format::Der => todo!(),
+        Format::Pem => todo!(),
+    }
+}
+
+/// Verify the cryptographical correctness of a given
+/// message value. This function returns an exit code that can be used to signal the result of the
+/// verification.
+fn verify_message(value: &str, format: Format) -> i32 {
     todo!()
+}
+
+/// Verify the well-formedness as well as the syntactical and cryptographical correctness of a given
+/// CSR value. This function returns an exit code that can be used to signal the result of the
+/// verification.
+fn verify_csr(value: &str, format: Format) -> i32 {
+    match format {
+        Format::Der => todo!(),
+        Format::Pem => todo!(),
+    }
+}
+
+/// Verify the well-formedness as well as the syntactical and cryptographical correctness of a given
+/// DN value. This function returns an exit code that can be used to signal the result of the
+/// verification.
+fn verify_dn(value: &str, format: Format) -> i32 {
+    match format {
+        Format::Der => todo!(),
+        Format::Pem => todo!(),
+    }
+}
+
+/// Verify the well-formedness as well as the syntactical and cryptographical correctness of a given
+/// RDN value. This function returns an exit code that can be used to signal the result of the
+/// verification.
+fn verify_rdn(value: &str, format: Format) -> i32 {
+    match format {
+        Format::Der => todo!(),
+        Format::Pem => todo!(),
+    }
 }
