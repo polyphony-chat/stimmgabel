9d201a11f07677199c57a325c2033d397f4b39a8
bitfl0wer <florian@pro-weber.com>
2024-05-15 12:14:27 +0200

    Impl verify certificate, verify csr

--- src/commands/verify.rs
+++ src/commands/verify.rs
@@ -2,7 +2,42 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use bitflags::Flags;
+use polyproto::certs::idcert::IdCert;
+use polyproto::certs::idcsr::IdCsr;
+use polyproto::errors::composite::ConversionError;
+use polyproto::Constrained;
+
 use crate::cli::{Format, StimmgabelMode, Target};
+use crate::errors::ExitCode;
+use crate::polyproto::keys::PublicKeyEd25519;
+use crate::polyproto::signature::SignatureEd25519;
+
+pub(crate) fn conversion_error_to_exit_code(error: ConversionError) -> i32 {
+    match error {
+        polyproto::errors::composite::ConversionError::ConstraintError(_) => {
+            ExitCode::CONSTRAINT_VIOLATION.bits()
+        }
+        polyproto::errors::composite::ConversionError::InvalidInput(_)
+        | polyproto::errors::composite::ConversionError::UnknownCriticalExtension { .. }
+        | polyproto::errors::composite::ConversionError::ConstOidError(_) => {
+            ExitCode::INVALID_INPUT.bits()
+        }
+        polyproto::errors::composite::ConversionError::DerError(_) => {
+            ExitCode::GARBLED_INPUT.bits()
+        }
+        polyproto::errors::composite::ConversionError::IdCertError(idcert_error) => {
+            return match idcert_error {
+                polyproto::errors::composite::PublicKeyError::BadSignature => {
+                    ExitCode::BAD_SIGNATURE.bits()
+                }
+                polyproto::errors::composite::PublicKeyError::BadPublicKeyInfo => {
+                    ExitCode::BAD_PUBLIC_KEY.bits()
+                }
+            }
+        }
+    }
+}
 
 /// Verify the well-formedness as well as the syntactical and cryptographical correctness of a given
 /// polyproto value. This function returns an exit code that can be used to signal the result of the
@@ -35,7 +70,22 @@ pub(crate) fn verify_input(mode: StimmgabelMode) -> i32 {
 /// certificate value. This function returns an exit code that can be used to signal the result of the
 /// verification.
 fn verify_certificate(value: &str, encoding: Format, target: Target) -> i32 {
-    todo!()
+    let certificate_result = match encoding {
+        Format::Der => IdCert::<SignatureEd25519, PublicKeyEd25519>::from_der(value.as_bytes()),
+        Format::Pem => IdCert::<SignatureEd25519, PublicKeyEd25519>::from_pem(value),
+    };
+    if let Err(error) = certificate_result {
+        return conversion_error_to_exit_code(error);
+    }
+    let certificate = certificate_result.unwrap();
+    let validation_result = match target {
+        Target::Actor => certificate.validate_actor(),
+        Target::Homeserver => certificate.validate_home_server(),
+    };
+    match validation_result {
+        Ok(_) => 0,
+        Err(error) => conversion_error_to_exit_code(error),
+    }
 }
 
 /// Verify the cryptographical correctness of a given
@@ -49,9 +99,21 @@ fn verify_message(value: &str) -> i32 {
 /// CSR value. This function returns an exit code that can be used to signal the result of the
 /// verification.
 fn verify_csr(value: &str, encoding: Format, target: Target) -> i32 {
-    match encoding {
-        Format::Der => todo!(),
-        Format::Pem => todo!(),
+    let csr_result = match encoding {
+        Format::Der => IdCsr::<SignatureEd25519, PublicKeyEd25519>::from_der(value.as_bytes()),
+        Format::Pem => IdCsr::<SignatureEd25519, PublicKeyEd25519>::from_pem(value),
+    };
+    if let Err(error) = csr_result {
+        return conversion_error_to_exit_code(error);
+    }
+    let csr = csr_result.unwrap();
+    let validation_result = match target {
+        Target::Actor => csr.validate_actor(),
+        Target::Homeserver => csr.validate_home_server(),
+    };
+    match validation_result {
+        Ok(_) => 0,
+        Err(error) => conversion_error_to_exit_code(error),
     }
 }
 
--- src/errors.rs
+++ src/errors.rs
@@ -7,12 +7,9 @@ use bitflags::bitflags;
 bitflags! {
     pub struct ExitCode: i32 {
         const GARBLED_INPUT = 1 << 0;
-        const INVALID_SPKI = 1 << 1;
-        const MISMATCHED_ISSUER_FOR_SUBJECT = 1 << 2;
-        const INVALID_CERTIFICATE_VERSION = 1 << 3;
-        const INVALID_CERTIFICATE_SIGNATURE = 1 << 4;
-        const UNKNOWN_SIGNATURE_ALGORITHM = 1 << 5;
-        const INVALID_EXTENSIONS_FOR_ACTOR = 1 << 6;
-        const INVALID_EXTENSIONS_FOR_HOMESERVER = 1 << 7;
+        const INVALID_INPUT = 1 << 1;
+        const CONSTRAINT_VIOLATION = 1 << 2;
+        const BAD_SIGNATURE = 1 << 3;
+        const BAD_PUBLIC_KEY = 1 << 4;
     }
 }
